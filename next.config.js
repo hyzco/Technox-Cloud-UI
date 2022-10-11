7f784d5f1bd9148ae20dfdeec9d9374711e4b774
hyzco <hsn.yzg@hotmail.com>
2022-10-12 00:03:23 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,29 +1,35 @@
-const path = require('path')
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 
 // Remove this if you're not using Fullcalendar features
-const withTM = require('next-transpile-modules')([
-  '@fullcalendar/common',
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/list',
-  '@fullcalendar/timegrid'
-])
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@fullcalendar/react",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/list",
+  "@fullcalendar/timegrid",
+]);
 
 module.exports = withTM({
   trailingSlash: true,
   reactStrictMode: false,
   experimental: {
     esmExternals: false,
-    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
+    jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
   },
-  webpack: config => {
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
+      apexcharts: path.resolve(
+        __dirname,
+        "./node_modules/apexcharts-clevision"
+      ),
+    };
 
-    return config
-  }
-})
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+});
