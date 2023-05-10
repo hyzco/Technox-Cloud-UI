34338589de5c3c89490a923ed0c974f607ddba76
hyzco <hsn.yzg@hotmail.com>
2023-05-11 00:37:11 +0200

    backendconfig

--- src/configs/backendConfig.ts
+++ src/configs/backendConfig.ts
@@ -9,5 +9,5 @@ const api = config.protocol + "://" + config.server + ":" + config.port;
 export default {
   apiProperties: config,
   // api: `${config.protocol}://${config.server}:${config.port}`,
-  api: "http://backend.theycloud.com/",
+  api: "https://backend.theycloud.com",
 };
